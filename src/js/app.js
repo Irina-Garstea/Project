e27f41e46b67c212d50b1d72ff94fe96b02953ee
Irina Garstea <irina.garstea@gmail.com>
2022-02-04 17:23:32 +0200

    [dropdown] dropdowSection: add mobile version

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,11 @@
-import './header';
+import "./header";
+import "./dropdown";
 
 /* Import Styles */
-import '../scss/app.scss';
+import "../scss/app.scss";
 
-if (process.env.NODE_ENV !== 'production') {
-  console.log('Looks like we are in development mode!');
+if (process.env.NODE_ENV !== "production") {
+  console.log("Looks like we are in development mode!");
 }
 
 /* Document ready put your JavaScript code here */
---
+++ src/js/dropdown.js
@@ -0,0 +1,5 @@
+document.querySelector(".circle").addEventListener("click", () => {
+  document.querySelector(".vertical").classList.toggle("open");
+  // document.querySelector(".line-wrapper").classList.toggle("open");
+  document.querySelector(".dropdown-text").classList.toggle("open");
+});
