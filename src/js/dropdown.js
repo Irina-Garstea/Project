0eddf5fcd134f4f290de38d5c53bafa131453b55
Irina Garstea <irina.garstea@gmail.com>
2022-02-07 23:32:33 +0200

    [About, DropDown, Message] modified code

--- src/js/dropdown.js
+++ src/js/dropdown.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const dropdownBtn = document.querySelector(".dropdown-btn");
 let dropdownDescription = document.querySelector(".dropdown-description");
 
@@ -6,14 +5,5 @@ if (dropdownBtn) {
   dropdownBtn.addEventListener("click", () => {
     dropdownBtn.classList.toggle("is-active");
     dropdownDescription.classList.toggle("is-active");
-=======
-const dropdownBtn = document.querySelector('.dropdown-btn');
-let dropdownDescription = document.querySelector('.dropdown-description');
-
-if (dropdownBtn) {
-  dropdownBtn.addEventListener('click', () => {
-    dropdownBtn.classList.toggle('is-active');
-    dropdownDescription.classList.toggle('is-active');
->>>>>>> 3de60675c5649e87243fada13f69e7af9451c500
   });
 }
