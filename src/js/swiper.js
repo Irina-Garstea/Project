76a9113bcdfc5cfc1d9155c0c5728f866cdf14a2
Irina Garstea <irina.garstea@gmail.com>
2022-02-13 00:44:14 +0200

    [Journey] JourneySection: add animation for image an slide-info

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,10 +1,14 @@
-import Swiper, { Navigation, Pagination } from "swiper";
+import Swiper, { Navigation, Pagination, EffectFade } from "swiper";
 // import $ from 'jquery';
 let Years = ["1996-2004", "2005-2009", "2010-2014", "2015-2021", "2021-2030"];
 
 var swiper = new Swiper(".journey-slider", {
-  modules: [Pagination, Navigation],
-
+  modules: [Pagination, Navigation, EffectFade],
+  effect: "fade",
+  speed: 1000,
+  fadeEffect: {
+    crossFade: true,
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
