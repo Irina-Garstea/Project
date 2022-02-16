361364cc702c7156b04fe6eea7f8c401cedb4f11
Irina Garstea <irina.garstea@gmail.com>
2022-02-16 18:00:38 +0200

    [space] spaceSection: add html, css and js

--- src/js/swiper.js
+++ src/js/swiper.js
@@ -1,14 +1,16 @@
-import Swiper, { Navigation, Pagination, EffectFade } from "swiper";
+import Swiper, { Autoplay, Navigation, Pagination, EffectFade } from "swiper";
+
 let Years = ["1996-2004", "2005-2009", "2010-2014", "2015-2021", "2021-2030"];
 
-var swiper = new Swiper(".journey-slider", {
+const swiper = new Swiper(".journey-slider", {
   modules: [Pagination, Navigation, EffectFade],
   wrapperClass: "journey-container",
   effect: "fade",
-  speed: 250,
+  speed: 1000,
   fadeEffect: {
     crossFade: true,
   },
+  slidesPerView: 1,
 
   pagination: {
     el: ".journey-pagination",
@@ -21,6 +23,24 @@ var swiper = new Swiper(".journey-slider", {
   },
 });
 
-var spaceswiper = new Swiper(".space-slider", {
-  modules: [Pagination, Navigation, EffectFade],
+const circleSlider = new Swiper(".circle-slider", {
+  modules: [Autoplay, EffectFade],
+  autoplay: {
+    delay: 1500,
+    waitForTransition: false,
+    disableOnInteraction: true,
+    virtualTranslate: true,
+  },
+  effect: "fade",
+  fadeEffect: { crossFade: true },
+  allowTouchMove: false,
+  on: {
+    slideChange: function () {
+      const index_currentSlide = circleSlider.realIndex;
+      const bigCircle = document.querySelector(".big-circle");
+      let removeIndex = index_currentSlide === 0 ? 11 : index_currentSlide - 1;
+      bigCircle.classList.add("big-circle-" + index_currentSlide);
+      bigCircle.classList.remove("big-circle-" + removeIndex);
+    },
+  },
 });
